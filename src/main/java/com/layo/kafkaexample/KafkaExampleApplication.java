4b785c7c4c699ff6b84ee349d33d27b7989d5a15
hetoney <toney.he@utoronto.ca>
2020-12-24 14:06:12 -0500

    tested

--- src/main/java/com/layo/kafkaexample/KafkaExampleApplication.java
+++ src/main/java/com/layo/kafkaexample/KafkaExampleApplication.java
@@ -4,6 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * Spring Boot app with Apache Kafka in Docker container (more detail Doc on https://habr.com/en/post/529222/)
+ * (Apache Kafka + Zookeeper docker image selection  --- images from wurstmeister, see https://hub.docker.com/r/wurstmeister/kafka )
+ *
+ * 1. Open spring initializr and create spring boot application with following dependencies:
+ *          Spring for Apache Kafka
+ *          Spring Web
+ *
+ * 2. Add application.yml file
+ *
+ * 3. KafKa Producer Service class uses autowired KafkaTemplate to send message to configured topic name.
+ *    Similarly, KafKa Consumer Service class uses @KafkaListener to receive messages from configured topic name.
+ *
+ * 4. Copy The 2 files below, into /etc/kafka/ folder
+ *          docker-compose.yml
+ *          kafka_server_jaas.conf
+ *      In that directory call:    docker-compose up -d
+ *
+ *  5. Start this app and see console
+ */
 @EnableScheduling
 @SpringBootApplication
 public class KafkaExampleApplication {
